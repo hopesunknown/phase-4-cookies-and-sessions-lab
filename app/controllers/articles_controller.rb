029a0e65a25e196f6ec70d7f08e693e84303d00d
Jennifer Tchai <jentchai@gmail.com>
2022-03-01 11:57:51 -0500

    commit tree e0a49b87095fb3ccd2248918cc45b58b297f9303

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,19 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    # set page views to initial value of zero
+    session[:page_views] ||=0
+    # for every request, increment the value by 1
+    session[:page_views] +=1
+
+    # if user viewed 3 or fewer pages, show article
+    # if more than 3 pages, include error message and 401 unauthorized
+    if session[:page_views] <= 3
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
 
   private
